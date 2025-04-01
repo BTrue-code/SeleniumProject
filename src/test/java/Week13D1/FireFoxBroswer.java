b9ed23e176b9408cdaa3efe56d5fe5feaaedf64f
BTrue-code <b.true1990@gmail.com>
2025-04-01 19:24:16 -0400

    Week 13 D1 - completed

--- src/test/java/Week13D1/FireFoxBroswer.java
+++ src/test/java/Week13D1/FireFoxBroswer.java
@@ -1,4 +1,22 @@
 package Week13D1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class FireFoxBroswer {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://www.syntaxprojects.com/");
+        String url = driver.getCurrentUrl();
+        String title = driver.getTitle();
+        System.out.println(url);
+        System.out.println(title);
+
+        driver.close();
+    }
+
 }
--- src/test/java/Week13D1/WebDriverCommands.java
+++ src/test/java/Week13D1/WebDriverCommands.java
@@ -1,4 +1,28 @@
 package Week13D1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WebDriverCommands {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://www.google.com");
+        String title1 = driver.getTitle();
+        System.out.println(title1);
+
+        driver.navigate().to("https://www.syntaxprojects.com/");
+        String title2 = driver.getTitle();
+        System.out.println(title2);
+
+        driver.navigate().back();
+
+        driver.navigate().refresh();
+
+        driver.close();
+    }
+
 }
